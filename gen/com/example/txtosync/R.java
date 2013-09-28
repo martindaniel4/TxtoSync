11c8648c7a8400bbe52840d4dc5468ed61c2525f
Martin Daniel <martindaniel4@gmail.com>
2013-09-28 17:40:10 +0200

    Change bacgkround

--- gen/com/example/txtosync/R.java
+++ gen/com/example/txtosync/R.java
@@ -10,6 +10,9 @@ package com.example.txtosync;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int steelblue=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,16 +25,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo_white=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int welcome=0x7f080000;
+        public static final int action_settings=0x7f090002;
+        public static final int buttonSync=0x7f090000;
+        public static final int imageView1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -60,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/txtosync/MainActivity.java
+++ src/com/example/txtosync/MainActivity.java
@@ -5,15 +5,11 @@ import android.os.Bundle;
 import android.widget.TextView;
  
 public class MainActivity extends Activity {
-  TextView welcome = null;
  
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
    
-         
-    welcome = (TextView)findViewById(R.id.welcome);
-    
     setContentView(R.layout.activity_main);
   }
  

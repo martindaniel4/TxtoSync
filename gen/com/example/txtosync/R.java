b23d81f93f6e8903fba10e9451bb2e010bcff3f9
Martin Daniel <martindaniel4@gmail.com>
2013-10-26 17:59:10 +0200

    Add logo to login page

--- gen/com/example/txtosync/R.java
+++ gen/com/example/txtosync/R.java
@@ -29,15 +29,16 @@ public final class R {
         public static final int txto_logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int buttonSync=0x7f090005;
+        public static final int action_settings=0x7f090009;
+        public static final int buttonSync=0x7f090006;
         public static final int email=0x7f090000;
         public static final int error=0x7f090003;
+        public static final int imageView1=0x7f090004;
         public static final int login=0x7f090002;
-        public static final int logoTxto=0x7f090004;
+        public static final int logoTxto=0x7f090005;
         public static final int password=0x7f090001;
-        public static final int smsDesc=0x7f090006;
-        public static final int smsMsg=0x7f090007;
+        public static final int smsDesc=0x7f090007;
+        public static final int smsMsg=0x7f090008;
     }
     public static final class layout {
         public static final int login=0x7f030000;
